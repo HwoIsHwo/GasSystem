af8672b93832775ba4be7047b7db85ef2d638a7c
Andrey Levshin <ael15@tpu.ru>
2024-04-20 17:32:18 +0700

    feat: Added search for time constants in the 2024/filters file. Added the ability to control the discharge in 2024\Modeling fix\MATLAB_function_model_new_disch.m and 2024\Modeling fix\Sfunction_model_new.m

---
+++ 2024/Modeling fix/minimal_seeking_main.m
@@ -0,0 +1,79 @@
+%% Чтение данных
+DATA = readmatrix('../Filtered_DATA.xlsx'); %чтение данных
+t = DATA(:,1); %запись в массив времени
+Y_exp = DATA(:,2);%запись в массив эксперементальных данных
+G = readmatrix('G.xlsx');
+G = G(:, 2);
+Dis = zeros(length(G), 1);
+Dis(37502:150000) = 1;
+
+
+%% "Обрезка" данных (каждый 10й элемент)
+t = t(1:10:end);
+Y_exp = Y_exp(1:10:end);
+G = G(1:10:end);
+Dis = Dis(1:10:end);
+
+U = [G, Dis];
+
+%%
+%Массив шагов для каждого из параметров (кол-во значений = кол-во парметров)
+h = [0.0001, 0.0001, 0.0001, 1e+16];
+%Матрица ограничений для каждого из параметров (мин и макс значения)
+%(кол-во значений = кол-во парметров)
+limit = [0, 5;
+             0, 5;
+             0, 5;
+             0, 1e+23;];
+
+%Количество итераций
+iter = 10000;
+
+% Начальные параметры
+t_retention = 0.0177;
+t_release = 0.6889;
+t_ion = 0.0338;
+N_w0 = 1.0469e20;
+par = [t_retention, t_release, t_ion, N_w0];
+
+%% Расчет (массив времени, параметры, ограничения параметров, входные переменные, 
+% ИМЯ функции, целевая функция (массив данных), коли-во итераций, массив шагов)
+%par = MATLAB_min_seek(par, G.', y, t, @scetch);
+%par = [t_p, t_retention, t_release, t_ion, N_v0, N_w0];
+par = minimal_seeking_function(t, par, limit, U, @MATLAB_function_model_new_disch, Y_exp, iter, h, 0);
+
+%% Вывод графиков
+y_mod = MATLAB_function_model_new_disch(t, par, U);
+figure;
+plot(t, Y_exp, 'DisplayName', 'Экспериментальные данные');
+hold on;
+plot(t, y_mod, 'DisplayName', 'Модель');
+plot(t, G, 'DisplayName', 'Газонапуск');
+xlabel('Время');
+ylabel('Кол-во частиц плазмы');
+legend('show');
+title('Сравнение экспериментальных данных и модели');
+grid on;
+
+% clc;
+% I = sum(abs(y-y_mod));
+% disp(I);
+% Aver = I / length(t);
+% disp(Aver);
+% Norm = Aver / max(y);
+% disp(Norm);
+
+% figure;
+% plot(t, G, 'DisplayName', 'Газонапуск');
+% xlabel('Время');
+% ylabel('Кол-во частиц плазмы');
+% legend('show');
+% grid on;
+% hold on;
+
+% plot(t, scetch(t, par, G.'));
+% hold on;
+% plot(t, y);
+% grid on;
+% legend('Y_{mod}', 'Y_{exp}');
+% hold off;
--- 2024/filters.m
+++ 2024/filters.m
@@ -40,7 +40,7 @@ grid on;
 level = 1000;   %Размер окна - чем больше, тем больше сглаживает
 aver_OUT = smoothdata(OUT, 'movmean', level);
 figure;
-plot(t,aver_OUT);
+plot(t, aver_OUT);
 legend('Aver');
 legend('boxoff');
 hold on;
@@ -64,9 +64,9 @@ legend('show');
 grid on;
 
 
-%% Аппроксимация полиномом (+)
+%% Аппроксимация полиномом (+-)
 n = 200;  %Порядок полинома
-p = polyfit(t, OUT, n); %Вместо OUT (изначальные данные) можно попробовать подставлять 
+p = polyfit(t, aver_OUT, n); %Вместо OUT (изначальные данные) можно попробовать подставлять 
 % уже отфильтрованные каким-нибудь фильтром
 poly_OUT = polyval(p, t);
 figure;
@@ -80,7 +80,7 @@ legend('show');
 grid on;
 
 
-%% Аппроксимация экспонентой (-)
+%% Аппроксимация экспонентой (--)
 X = [ones(size(t)) exp(-t) t.*exp(-t) (t.^2).*exp(-t) (t.^3).*exp(-t) (t.^4).*exp(-t) (t.^5).*exp(-t) (t.^6).*exp(-t)];
 a = X\OUT;
 exp_OUT = [ones(size(t)) exp(-t) t.*exp(-t) (t.^2).*exp(-t) (t.^3).*exp(-t) (t.^4).*exp(-t) (t.^5).*exp(-t) (t.^6).*exp(-t)]*a;
@@ -110,3 +110,63 @@ xlabel('Время, с');
 ylabel('Напряжение, В');
 legend('show');
 grid on;
+
+
+%% Определение постоянной времени (задний фронт)
+a = 146000; % Выбираем точки, с которых по которым строи график
+b = 168757;
+T = t(a:b)-t(a);
+Y = aver_OUT(a:b)-min(aver_OUT(a:b));
+
+fun1 = @(x, xdata) x(1)*exp(x(2)*xdata); % Эспоненциальная функция для апроксимации
+
+par = [1.6e+19, -1];
+new_par = lsqcurvefit(fun1, par, T, Y);
+t_p = -1/new_par(2);
+
+figure;
+plot(T, Y, T, fun1(new_par, T));
+legend('Data','Fitted exponential');
+hold on;
+grid on;
+
+
+%% Определение постоянной времени (передний фронт, используется часть модели)
+a = 47500; % Выбираем точки, с которых по которым строим график
+b = 110000;
+T = t(a:b)-t(a);
+Y = aver_OUT(a:b)-0.4e+19;
+
+fun2 = @(x, xdata) 2*t_p*0.6e19/x-(4.728e17/x)*exp(-xdata/t_p);
+
+par = 1;
+new_par = lsqcurvefit(fun2, par, T, Y);
+t_ion = new_par;
+
+figure;
+plot(T, Y, T, fun2(new_par, T));
+legend('Data','Fitted exponential');
+hold on;
+grid on;
+
+%% Передний фронт (просто экспонента)
+a = 44000; % Выбираем точки, с которых по которым строим график
+b = 120000;
+T = t(a:b)-t(a);
+Y = aver_OUT(a:b)-0.4e+19;
+
+fun3 = @(x, xdata) x(1)*(1-exp(x(2)*xdata)); % Эспоненциальная функция для апроксимации
+
+par = [1.6e+19, -1];
+new_par = lsqcurvefit(fun3, par, T, Y);
+t_ion = -1/new_par(2);
+
+figure;
+plot(T, Y, T, fun3(new_par, T));
+legend('Data','Fitted exponential');
+hold on;
+grid on;
+
+
+%% Запись отфильтрованных данных в файл
+writematrix([t, aver_OUT], 'Filtered_DATA.xlsx');
